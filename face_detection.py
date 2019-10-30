b2ddf73c181e99570d8c17f4223ff8d541720d0f
Ignacio Rittano <rittanoignacio@gmail.com>
2019-10-30 16:28:41 -0300

    Class FaceDetector

--- face_detection.py
+++ face_detection.py
@@ -3,21 +3,100 @@ import numpy as np
 import video
 import pygame
 
+class FaceDetector():
+
+    def __init__(self):
+
+        self.video_capture = cv2.VideoCapture(0)
+        cascPath = "res/haarcascade_frontalface_alt.xml"
+        self.faceCascade = cv2.CascadeClassifier(cascPath)
+        self.cam_width = self.video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.cam_height = self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.faces = []
+
+    def face_detection(self, modo = False):
+
+        ret, frame = self.video_capture.read()
+
+        if len(self.faces) > 0:
+
+            face = self.faces[0]
+
+            x1 = max((int(face[0]-face[2]*0.2), 0))
+            y1 = max((int(face[1]-face[3]*0.2), 0))
+            x2 = min((int(face[0]-face[2]*1.2), self.cam_width))
+            y2 = min((int(face[1]-face[3]*1.2), self.cam_height))
+
+            width = x2-x1
+            heigth = y2-y1
+
+            crop_img = frame[x1:x1+width, y1:y1+heigth]
+            print("FaceDetector")
+            print(self.faces)
+            self.faces = self.faceCascade.detectMultiScale(
+                crop_img,
+                scaleFactor=1.2,
+                minNeighbors=15,
+                minSize=(50, 50),
+                #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+            print(self.faces)
+            self.faces[0] += x1
+            self.faces[1] += y1
+
+        else:
+
+            self.faces = self.faceCascade.detectMultiScale(
+                frame,
+                scaleFactor=1.2,
+                minNeighbors=15,
+                minSize=(50, 50),
+                #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+
+        if modo == False:
+
+            for (x, y, w, h) in self.faces:
+                left_eye = (int(x+0.30*w), int(y+0.37*h))
+                right_eye = (int(x+0.70*w), int(y+0.37*h))
+                eyes_center = (int(x+0.5*w), int(y+0.37*h))
+                return eyes_center, w, h
+
+        else:
+            if len(self.faces) == 0:
+                return frame
+
+            else:
+                face = self.faces[0]
+                x = int(face[0])
+                y = int(face[1])
+                a = int(face[2])
+                h = int(face[3])
+                left_eye =(int(x+0.3*a) , int(y+0.37*h))
+                right_eye = (int(x+0.7*a), int(y+0.37*h))
+
+                face = cv2.rectangle(frame,(x, y),(x+a,y+h),(255,0,0),2)
+                cv2.circle(frame, left_eye, int(0.05*a), (255,255,255), 2)
+                cv2.circle(frame, right_eye, int(0.05*a), (255,255,255), 2)
+
+                return frame
+
+    def __del__(self):
+
+        self.video_capture.release()
+        cv2.destroyAllWindows()
+
 
 def main(prm, default_prm, args):
 
-    eyesGap = 6.5
-
-    video_capture = cv2.VideoCapture(0)
-
     #cam_width = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
     #cam_height = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    face_detector = FaceDetector()
 
     while True:
 
-        ret, frame = video_capture.read()
-        face_detection(frame, modo = True)
-
+        frame = face_detector.face_detection(modo = True)
+        #print(type(frame))
         cv2.imshow('Video', cv2.flip(frame,1))
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -25,35 +104,3 @@ def main(prm, default_prm, args):
 
     video_capture.release()
     cv2.destroyAllWindows()
-
-def face_detection(frame, modo = False):
-
-    cascPath = "res/haarcascade_frontalface_alt.xml"
-    faceCascade = cv2.CascadeClassifier(cascPath)
-
-    faces = faceCascade.detectMultiScale(
-        frame,
-        scaleFactor=1.2,
-        minNeighbors=15,
-        minSize=(50, 50),
-        #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-    if modo == False:
-
-        for (x, y, w, h) in faces:
-            left_eye = (int(x+0.30*w), int(y+0.37*h))
-            right_eye = (int(x+0.70*w), int(y+0.37*h))
-            eyes_center = (int(x+0.5*w), int(y+0.37*h))
-            return eyes_center, w, h
-
-    else:
-
-        for (x, y, w, h) in faces:
-            face = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            left_eye = (int(x+0.30*w), int(y+0.37*h))
-            right_eye = (int(x+0.70*w), int(y+0.37*h))
-
-            cv2.circle(frame, (left_eye), int(0.05*w), (255,255,255),2)
-            cv2.circle(frame, (right_eye), int(0.05*w), (255,255,255),2)
-
-            return frame
