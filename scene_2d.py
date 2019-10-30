a0346c084962d7f66e6cc0615bd0321ac513fe12
Nano Remedi <agustoremedi@gmail.com>
2019-10-30 16:18:54 -0300

    escena 2d mejorada

--- scene_2d.py
+++ scene_2d.py
@@ -27,8 +27,10 @@ class Image:
         self.scaling_factor = scaling_factor
         self.move_ratio = move_ratio
 
-        self.image = pygame.image.load(path).convert_alpha()
-        self.scaled_img = pygame.transform.scale(self.image, (window * scaling_factor).astype(int))
+        image = pygame.image.load(path).convert_alpha()
+        w = window[0] * scaling_factor
+        h = ( w / image.get_rect().size[0] ) * image.get_rect().size[1]
+        self.scaled_img = pygame.transform.scale(image, (int(w), int(h)))
 
     def draw_image(self, mouse, window, screen):
         #Toma el cursor de pygame y las dimensiones de la pantalla en forma
@@ -49,15 +51,15 @@ def main(prm, default_prm, args):
 
 
     sprites = [ Image("./res/casa_tucuman_16_9.jpg", 0.5, 1.3, 0.3, window_s),
-                Image("./res/moreno.png", (1.05, 0.8), (0.15, 0.3), 0.29, window_s),
-                Image("./res/paso.png", (0.99, 0.8), (0.2, 0.3), 0.29, window_s),
-                Image("./res/larrea.png", (-0.05, 0.85), (0.3, 0.4), 0.25, window_s),
-                Image("./res/matheu.png", (0.08, 0.8), (0.3, 0.4), 0.25, window_s),
-                Image("./res/alberti.png", (0.85, 0.8), (0.3, 0.4), 0.17, window_s),
-                Image("./res/azcuenaga.png", (0.2, 0.8), (0.3, 0.4), 0.17, window_s),
-                Image("./res/belgrano.png", (0.7, 0.75), (0.3, 0.7), 0.125, window_s),
-                Image("./res/castelli.png", (0.3, 0.75), (0.3, 0.5), 0.125, window_s),
-                Image("./res/saavedra.png", (0.5, 0.65), (0.5, 0.7), 0.05, window_s),
+                Image("./res/moreno.png", (1.1, 0.8), 0.15, 0.29, window_s),
+                Image("./res/paso.png", (1.025, 0.8), 0.2, 0.29, window_s),
+                Image("./res/larrea.png", (-0.05, 0.85), 0.3, 0.25, window_s),
+                Image("./res/matheu.png", (0.08, 0.8), 0.3, 0.25, window_s),
+                Image("./res/alberti.png", (0.85, 0.8), 0.3, 0.17, window_s),
+                Image("./res/azcuenaga.png", (0.2, 0.8), 0.3, 0.17, window_s),
+                Image("./res/belgrano.png", (0.7, 0.75), 0.3, 0.125, window_s),
+                Image("./res/castelli.png", (0.3, 0.75), 0.3, 0.125, window_s),
+                Image("./res/saavedra.png", (0.5, 0.9), 0.5, 0.05, window_s),
             ]
 
     def loop(screen, delta_t, window_w, window_h):
